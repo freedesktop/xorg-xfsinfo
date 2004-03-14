deba072592938eb8f684756831fef1c03247f187
Egbert Eich <eich@suse.de>
2004-03-14 08:35:23 +0000

    Importing vendor version xf86-4_4_99_1 on Sun Mar 14 00:26:39 PST 2004

--- xfsinfo.c
+++ xfsinfo.c
@@ -30,7 +30,7 @@ other dealings in this Software without prior written authorization
 from The Open Group.
 
 */
-/* $XFree86: xc/programs/xfsinfo/xfsinfo.c,v 1.3 2001/08/27 17:41:02 dawes Exp $ */
+/* $XFree86: xc/programs/xfsinfo/xfsinfo.c,v 1.4 2001/12/14 20:01:42 dawes Exp $ */
 
 /*
  * Copyright 1990 Network Computing Devices;
